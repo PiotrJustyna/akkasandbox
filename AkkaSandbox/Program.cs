65d64b82938520bd7e707349c23654fbf61f4963
Piotr Justyna <piotr.justyna@gmail.com>
2020-04-07 20:10:08 +0100

    Actors in a generic hosted service.

--- AkkaSandbox/Program.cs
+++ AkkaSandbox/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Akka.Actor;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace AkkaSandbox
 {
@@ -7,10 +11,35 @@ namespace AkkaSandbox
     {
         static void Main(string[] args)
         {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder(args)
+        .ConfigureServices((hostContext, services) =>
+        {
+            services.AddHostedService<HelloWorldHostedService>();
+        });
+    }
+
+    public class HelloWorldHostedService : IHostedService
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Starting the host.");
+
             var system = ActorSystem.Create("testSystem");
             var firstRef = system.ActorOf(Props.Create<PrintMyActorRefActor>(), "first-actor");
             Console.WriteLine($"First: {firstRef}");
             firstRef.Tell("printit", ActorRefs.NoSender);
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Stopping the host.");
+            return Task.CompletedTask;
         }
     }
 }
